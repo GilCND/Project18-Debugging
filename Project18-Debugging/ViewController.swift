594fc62c8dee526a099db26a99974a80eb841c0f
GilCND <fgilcnd@gmail.com>
2021-08-30 13:46:17 -0300

    [Project18-Debugging] Example code added

--- Project18-Debugging/ViewController.swift
+++ Project18-Debugging/ViewController.swift
@@ -11,9 +11,27 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //Using Print
+//        print("I', inside the viewDidLoad")
+//        print(1, 2, 3, 4, 5, 6, separator: "-")
+//        print("Some message", terminator: "")
+//
+        //Using Assert
+//        assert(1 == 1, "Math failure!")
+//        assert(1 == 2, "Math failure!")
+//        assert(myReallySlowMethod() == true, "The slow method returned false, which is a bad thing.")
+        
+        //Using Breakpoints
+//        for i in 1...100 {
+//            print("Got number \(i).")
+//        }
+        
     }
-
+    private func myReallySlowMethod() -> Bool {
+        let variable = true
+        return variable
+    }
 
 }
 
